e2279f2c7557f2b539d8b7ee69348f3cbb1d4c0f
Oleg Repin <oy.repin@bk.ru>
2022-08-27 22:24:11 +0300

    Add solution for ODD

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,14 +4,15 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers):
+
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-
+    return[number ** 2 for number in numbers]
 
 # filter types
 ODD = "odd"
@@ -19,7 +20,7 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(numbers_list, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +31,5 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if filter_type == ODD:
+        return [number for number in numbers_list if number % 2 != 0]
